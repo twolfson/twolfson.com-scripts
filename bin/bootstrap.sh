a474dc624320a64f08b6ee5f1ac9b1dbbba1c60e
Todd Wolfson <todd@twolfson.com>
2015-12-08 22:43:50 -0600

    Exploring robust dpkg logic

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -3,18 +3,22 @@
 set -e
 set -x
 
-# If we haven't updated apt-get, then update it now
-# TODO: Use timestamp to update it on a schedule (e.g. 1 day)
+# Collect current timestmap in seconds (e.g. 1449635654)
+timestamp="$(date +%s)"
+
+# If we haven't updated apt-get or it's been a day since our last apt-get update, then update it now
 # TODO: Maybe build a function like `update_apt_get` used by other functions?
-if ! test -f .updated-apt-get; then
+if (! test -f .updated-apt-get) || test "$timestamp" -ge "$(($(cat .updated-apt-get) + (60 * 60 * 24)))"; then
   sudo apt-get update
-  touch .updated-apt-get
+  echo -n "$timestamp" > .updated-apt-get
 fi
 
-# If we haven't installed note, then set it up
+# If we haven't installed node or it's out of date, then set it up
 # https://github.com/nodesource/distributions/tree/96e9b7d40b6aff7ade7bc130d9e18fd140e9f4f8#installation-instructions
-# TODO: Upgrade to newer version of node
+node_version="0.10.41-1nodesource1~trusty1"
 if ! which node &> /dev/null; then
   curl -sL https://deb.nodesource.com/setup_0.10 | sudo -E bash -
+fi
+if (! which node &> /dev/null) || (! dpkg --list | grep nodejs | grep "$node_version"); then
   sudo apt-get install -y "nodejs=0.10.41-1nodesource1~trusty1"
 fi
