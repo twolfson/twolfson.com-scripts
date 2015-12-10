2ee80dd6427837df2f38a90753ecf9623cf2fb94
Todd Wolfson <todd@twolfson.com>
2015-12-10 00:12:39 -0600

    Simplified our spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,42 +1,21 @@
 # Load in our dependencies
 require "serverspec"
 require "net/ssh"
-require "tempfile"
 
-set :backend, :ssh
+# Load in our environment variable to the SSH config
+ssh_config = ENV.fetch("SSH_CONFIG")
+host = ENV.fetch("TARGET_HOST")
 
-if ENV["ASK_SUDO_PASSWORD"]
-  begin
-    require "highline/import"
-  rescue LoadError
-    fail "highline is not available. Try installing it."
-  end
-  set :sudo_password, ask("Enter sudo password: ") { |q| q.echo = false }
-else
-  set :sudo_password, ENV["SUDO_PASSWORD"]
-end
+# Extract the SSH setup for our server
+#   Host default
+#     HostName 127.0.0.1
+#     User vagrant
+#     Port 2222
+# into:
+#   {host_name, user, port, ...}
+options = Net::SSH::Config.for(host, [ssh_config])
 
-host = ENV["TARGET_HOST"]
-
-`vagrant up #{host}`
-
-config = Tempfile.new("", Dir.tmpdir)
-config.write(`vagrant ssh-config #{host}`)
-config.close
-
-options = Net::SSH::Config.for(host, [config.path])
-
-options[:user] ||= Etc.getlogin
-
-set :host,        options[:host_name] || host
-set :ssh_options, options
-
-# Disable sudo
-# set :disable_sudo, true
-
-
-# Set environment variables
-# set :env, :LANG => "C", :LC_MESSAGES => "C"
-
-# Set PATH
-# set :path, "/sbin:/usr/local/sbin:$PATH"
+# Configure serverspec
+set(:backend, :ssh)
+set(:host, options[:host_name])
+set(:ssh_options, options)
