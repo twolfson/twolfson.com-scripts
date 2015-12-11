3e502a129109574d7501ba02531ff58175fc4e36
Todd Wolfson <todd@twolfson.com>
2015-12-11 01:22:07 -0600

    Set up user shell adjustments

--- bin/_bootstrap.sh
+++ bin/_bootstrap.sh
@@ -59,3 +59,18 @@ if test "$(ls /etc/nginx/sites-enabled)" != ""; then
   # Reload the NGINX server
   sudo /etc/init.d/nginx reload
 fi
+
+# If the root user has a non-nologin login shell, update it
+# https://github.com/mizzy/specinfra/blob/v2.44.7/lib/specinfra/command/base/user.rb#L53-L55
+# https://github.com/mizzy/specinfra/blob/v2.44.7/lib/specinfra/command/base/user.rb#L61-L63
+if test "$(getent passwd root | cut -f 7 -d ":")" != "/usr/sbin/nologin"; then
+  sudo usermod --shell /usr/sbin/nologin root
+fi
+
+# If the sync user has a non-nologin login shell, update it
+# DEV: The sync user exists to run `sync` on a server without needing to use a shell
+#   However, we are going to be paranoid and remove it
+# http://www.unix.com/fedora/167621-user-sync-shutdown.html
+if test "$(getent passwd sync | cut -f 7 -d ":")" != "/usr/sbin/nologin"; then
+  sudo usermod --shell /usr/sbin/nologin sync
+fi
