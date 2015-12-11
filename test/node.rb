29cd03aaa1035df9ed880905e233385b7dd96f53
Todd Wolfson <todd@twolfson.com>
2015-12-10 22:40:17 -0600

    Added Node.js tests

---
+++ test/node.rb
@@ -0,0 +1,13 @@
+# Load in our dependencies
+require_relative "utils/serverspec"
+
+# Start our tests
+describe "Node.js" do
+  it "is installed via apt" do
+    expect(package("nginx")).to(be_installed())
+  end
+
+  it "has our expected version" do
+    expect(command("node --version").stdout).to(eq("v0.10.41\n"))
+  end
+end
