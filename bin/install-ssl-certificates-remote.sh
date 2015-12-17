b5886666b1bba2beaa56a9e3a342477c45092374
Todd Wolfson <todd@twolfson.com>
2015-12-17 00:12:15 -0600

    Tried out getopts but dislike single arguments only

--- bin/install-ssl-certificates-remote.sh
+++ bin/install-ssl-certificates-remote.sh
@@ -3,15 +3,22 @@
 set -e
 
 # If there is no remote server to build to, then complain and leave
-usage_str="Usage: SOURCE_KEY=\"my-domain.key\" SOURCE_CRT=\"my-domain.crt\" TARGET_HOST=\"my-server-from-ssh-config\" bin/install-ssl-certificates-remote.sh"
-if test "$TARGET_HOST" = ""; then
-  # DEV: We support `TARGET_HOST="my-user@127.0.0.1"` as well but that's inconsistent with `test-remote.sh`
-  echo "Environment variable \`TARGET_HOST\` was not set. Please set it before running \`install-ssl-certificates-remote.sh\`" 1>&2
-  echo "$usage_str" 1>&2
-  exit 1
-fi
+# DEV: We support `TARGET_HOST="my-user@127.0.0.1"` as well but that's inconsistent with `test-remote.sh`
+usage_str="Usage: $0 \"name-of-host-in-ssh-config\" --crt \"path/to/domain.crt\" --key \"path/to/domain.key\""
+target_host="$1"
+# shift
+# if test "$target_host" = ""; then
+#   echo "Environment variable \`TARGET_HOST\` was not set. Please set it before running \`$0\`" 1>&2
+#   echo "$usage_str" 1>&2
+#   exit 1
+# fi
 
 # If we don't have a certificate key or bundle
+# http://superuser.com/a/541673
+while getopts crt:key: OPT; do
+  echo "$OPT"
+  echo "$OPTARG"
+done
 
 # Output all future commands
 set -x
