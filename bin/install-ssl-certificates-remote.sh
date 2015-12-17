89b5630d86e168aaed4b04dd2ca82f5ab93e4404
Todd Wolfson <todd@twolfson.com>
2015-12-17 00:22:32 -0600

    Moving to while/case for handling options

--- bin/install-ssl-certificates-remote.sh
+++ bin/install-ssl-certificates-remote.sh
@@ -6,19 +6,42 @@ set -e
 # DEV: We support `TARGET_HOST="my-user@127.0.0.1"` as well but that's inconsistent with `test-remote.sh`
 usage_str="Usage: $0 \"name-of-host-in-ssh-config\" --crt \"path/to/domain.crt\" --key \"path/to/domain.key\""
 target_host="$1"
-# shift
-# if test "$target_host" = ""; then
-#   echo "Environment variable \`TARGET_HOST\` was not set. Please set it before running \`$0\`" 1>&2
-#   echo "$usage_str" 1>&2
-#   exit 1
-# fi
+shift
+if test "$target_host" = ""; then
+  echo "Target host was not set. Please pass it as an argument to \`$0\`" 1>&2
+  echo "$usage_str" 1>&2
+  exit 1
+fi
 
-# If we don't have a certificate key or bundle
-# http://superuser.com/a/541673
-while getopts crt:key: OPT; do
-  echo "$OPT"
-  echo "$OPTARG"
+# Parse remaining arguments
+# http://tldp.org/LDP/Bash-Beginners-Guide/html/sect_07_03.html
+# http://superuser.com/a/541675
+while true; do
+  case "$1" in
+    --crt)
+      shift
+      crt_path="$1"
+      ;;
+    --key)
+      shift
+      key_path="$1"
+      ;;
+  esac
+  shift || break
 done
+
+# If we don't have a certificate key or bundle, then complain and leave
+echo "$key_path" "$crt_path"
+if test "$key_path" = ""; then
+  echo "Certificate key was not set. Please pass it as an argument (\`--key\`) to \`$0\`" 1>&2
+  echo "$usage_str" 1>&2
+  exit 1
+fi
+if test "$crt_path" = ""; then
+  echo "Certificate bundle was not set. Please pass it as an argument (\`--crt\`) to \`$0\`" 1>&2
+  echo "$usage_str" 1>&2
+  exit 1
+fi
 
 # Output all future commands
 set -x
