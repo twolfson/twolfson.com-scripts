ba775b11750f73d85b1059a832aca3532b6bc107
Todd Wolfson <todd@twolfson.com>
2015-12-22 02:19:15 -0600

    Fixed up whitespace

--- bin/_bootstrap.sh
+++ bin/_bootstrap.sh
@@ -88,7 +88,7 @@ fi
 
 # If there are no NGINX configuration files, then install them
 if test "$(cat /etc/nginx/conf.d/twolfson.com.conf 2> /dev/null)" != "$(cat "$data_dir/etc/nginx/conf.d/twolfson.com.conf")") ||
-   test "$(cat /etc/nginx/nginx.conf 2> /dev/null)" != "$(cat "$data_dir/etc/nginx/nginx.conf")"; then
+    test "$(cat /etc/nginx/nginx.conf 2> /dev/null)" != "$(cat "$data_dir/etc/nginx/nginx.conf")"; then
   # Install our configurations
   sudo chown root:root "$data_dir/etc/nginx/conf.d/twolfson.com.conf"
   sudo chmod u=rw,g=r,o=r "$data_dir/etc/nginx/conf.d/twolfson.com.conf"
