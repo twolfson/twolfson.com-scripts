c07cbda16344bfabcc8efef3f98ad1c70e675e1d
Todd Wolfson <todd@twolfson.com>
2015-12-24 02:29:19 -0600

    Fixed up errors from timezone configuration

--- bin/_bootstrap.sh
+++ bin/_bootstrap.sh
@@ -25,8 +25,12 @@ fi
 
 # If the timezone isn't as we expect, then update it now
 # https://www.digitalocean.com/community/questions/how-to-change-the-timezone-on-ubuntu-14
+# http://serverfault.com/a/84528
 if test "$(date +"%z")" != "+0000"; then
-  sudo dpkg-reconfigure tzdata
+  sudo chown root:root "$data_dir/etc/timezone"
+  sudo chmod u=rw,g=r,o=r "$data_dir/etc/timezone"
+  sudo cp --preserve "$data_dir/etc/timezone" /etc/timezone
+  sudo dpkg-reconfigure --frontend noninteractive tzdata
 fi
 
 # If there is no ubuntu user, then create them
--- test/system.rb
+++ test/system.rb
@@ -8,4 +8,11 @@ describe "System" do
     expect(timezone_result.exit_status).to(eq(0))
     expect(timezone_result.stdout).to(eq("+0000" + "\n"))
   end
+
+  it "has proper permissions set for timezone file" do
+    timezone_file = file("/etc/timezone")
+    expect(timezone_file.mode).to(eq((USER_RW | GROUP_R | OTHER_R).to_s(8)))
+    expect(timezone_file.owner).to(eq(ROOT_USER))
+    expect(timezone_file.group).to(eq(ROOT_GROUP))
+  end
 end
