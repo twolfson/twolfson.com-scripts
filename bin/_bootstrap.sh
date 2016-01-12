6814df99c0ffcfb418f21fd10e430e13fea4140d
Todd Wolfson <todd@twolfson.com>
2016-01-11 23:53:06 -0600

    Added bootstrap blocker for now

--- bin/_bootstrap.sh
+++ bin/_bootstrap.sh
@@ -14,6 +14,10 @@ if test "$data_dir" = ""; then
   exit 1
 fi
 
+# TODO: Remove me when Chef dev is over
+echo "Development for Chef going on" 1>&2
+exit 0
+
 # If we haven't updated apt-get, then update it now
 # TODO: Use timestamp to update it on a schedule (e.g. 1 day)
 #   https://github.com/twolfson/twolfson.com-scripts/blob/150de4af2778e577ca3d57dab74b6dd7a0e1a55f/bin/bootstrap.sh#L6-L14
