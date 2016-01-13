8885ab0659689f711b7acd864e4a8f271e830526
Todd Wolfson <todd@twolfson.com>
2016-01-13 01:02:00 -0600

    Fixed up resolution but something is wrong

--- src/index.rb
+++ src/index.rb
@@ -1,4 +1,4 @@
 # Load in our other "recipes"
 # DEV: We avoid the traditional Chef structure due to it
 #   being overly complex and unnecessary for 1 node ecosystem
-require "apt.rb"
+require_relative "apt.rb"
