9e394dcf481463bcd03e7015269014a533d412ad
Todd Wolfson <todd@twolfson.com>
2016-01-14 01:57:47 -0600

    Fixed up offset but realizing we messed up File load

--- test/apt.rb
+++ test/apt.rb
@@ -4,7 +4,7 @@ require_relative "utils/serverspec"
 # Start our tests
 describe "apt" do
   it "was updated within past 24 hours" do
-    one_day_ago = 60 * 60 * 24
+    one_day_ago = Time.now() - 60 * 60 * 24
     last_update_timestamp = File.mtime("/var/lib/apt/periodic/update-success-stamp")
     expect(last_update_timestamp).to(be < one_day_ago)
     expect(package("bash")).to(be_installed())
