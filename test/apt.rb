219c4614fbccc3f157ede02fc468c2b78c9c2716
Todd Wolfson <todd@twolfson.com>
2016-01-14 02:01:23 -0600

    Corrected logic

--- test/apt.rb
+++ test/apt.rb
@@ -4,9 +4,8 @@ require_relative "utils/serverspec"
 # Start our tests
 describe "apt" do
   it "was updated within past 24 hours" do
-    one_day_ago = Time.now() - 60 * 60 * 24
-    last_update_timestamp = File.mtime("/var/lib/apt/periodic/update-success-stamp")
-    expect(last_update_timestamp).to(be < one_day_ago)
-    expect(package("bash")).to(be_installed())
+    one_day_ago = DateTime.now() - 60 * 60 * 24
+    timestamp_file = file("/var/lib/apt/periodic/update-success-stamp")
+    expect(timestamp_file.mtime).to(be >= one_day_ago)
   end
 end
