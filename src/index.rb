b45e3941eb9f6a0b65f6c94bccb3b653c44a21ae
Todd Wolfson <todd@twolfson.com>
2016-01-14 02:16:56 -0600

    Exploring running a custom apply invocation

--- src/index.rb
+++ src/index.rb
@@ -1,4 +1,20 @@
-# Load in our other "recipes"
+# Load in our dependencies
+# https://github.com/chef/chef/blob/12.6.0/bin/chef-apply#L21-L25
+require "rubygems"
+require "chef/application/apply"
+
+# Run our recipes
 # DEV: We avoid the traditional Chef structure due to it
 #   being overly complex and unnecessary for 1 node ecosystem
-require_relative "apt.rb"
+apply = Chef::Application::Apply.new()
+# recipe_filename = "apt.rb"
+# recipe_text, recipe_fh = read_recipe_file recipe_filename
+# recipe,run_context = get_recipe_and_run_context()
+# recipe.instance_eval(recipe_text, recipe_filename, 1)
+# runner = Chef::Runner.new(run_context)
+# begin
+#   runner.converge
+# ensure
+#   recipe_fh.close
+# end
+# Chef::Platform::Rebooter.reboot_if_needed!(runner)
