aeba48d37849636fab4845542e93c4b296b70e6c
Todd Wolfson <todd@twolfson.com>
2016-01-19 02:20:20 -0600

    Transferred node provisioner to Chef

--- src/twolfson.com.sh
+++ src/twolfson.com.sh
@@ -2,24 +2,10 @@
 # Exit on first error
 set -e
 
-# Run our common provisioners
+# Run our Chef provisioner
 cd src
 sudo data_dir="$data_dir" chef-client --local-mode --override-runlist "recipe[twolfson.com]"
 cd -
-
-# Define and run our provisioners
-# @depends_on: apt_provisioner
-node_provisioner() {
-  # If we haven't installed node, then set it up
-  # https://github.com/nodesource/distributions/tree/96e9b7d40b6aff7ade7bc130d9e18fd140e9f4f8#installation-instructions
-  # TODO: Handle out of date scenario
-  #   https://github.com/twolfson/twolfson.com-scripts/blob/150de4af2778e577ca3d57dab74b6dd7a0e1a55f/bin/bootstrap.sh#L18-L24
-  if ! which node &> /dev/null; then
-    curl -sL https://deb.nodesource.com/setup_0.10 | sudo -E bash -
-    sudo apt-get install -y "nodejs=0.10.41-1nodesource1~trusty1"
-  fi
-}
-node_provisioner
 
 # @depends_on: nginx_provisioner_common
 nginx_provisioner_twolfson_com() {
