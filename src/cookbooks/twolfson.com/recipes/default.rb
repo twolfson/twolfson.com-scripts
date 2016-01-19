a8034249e3eb1ee4dec2e2b807b4125dd76a4d16
Todd Wolfson <todd@twolfson.com>
2016-01-19 02:25:53 -0600

    Completed provisioner transition

--- src/cookbooks/twolfson.com/recipes/default.rb
+++ src/cookbooks/twolfson.com/recipes/default.rb
@@ -13,3 +13,49 @@ apt_package "nodejs" do
   version("0.10.41-1nodesource1~trusty1")
   only_if("test \"$(node --version)\" != \"v0.10.41\"")
 end
+
+# Configure NGINX for `twolfson.com` node
+# @depends_on service[nginx]
+file "/etc/nginx/conf.d/twolfson.com.conf" do
+  owner("root")
+  group("root")
+  mode("644") # u=rw,g=r,o=r
+
+  content(File.new("#{data_dir}/etc/nginx/conf.d/twolfson.com.conf").read())
+
+  # When we update, reload our NGINX
+  # DEV: We have a delay to guarantee all configs reload at the same time
+  notifies(:reload, "service[nginx]", :delayed)
+end
+file "/etc/nginx/conf.d/drive.twolfson.com.conf" do
+  owner("root")
+  group("root")
+  mode("644")
+  content(File.new("#{data_dir}/etc/nginx/conf.d/drive.twolfson.com.conf").read())
+  notifies(:reload, "service[nginx]", :delayed)
+end
+file "/etc/nginx/conf.d/twolfsn.com.conf" do
+  owner("root")
+  group("root")
+  mode("644")
+  content(File.new("#{data_dir}/etc/nginx/conf.d/twolfsn.com.conf").read())
+  notifies(:reload, "service[nginx]", :delayed)
+end
+file "/etc/nginx/nginx.conf" do
+  owner("root")
+  group("root")
+  mode("644")
+  content(File.new("#{data_dir}/etc/nginx/nginx.conf").read())
+  notifies(:reload, "service[nginx]", :delayed)
+end
+# Guarantee we have a folder for `drive.twolfson.com`
+directory "/var/www" do
+  owner("root")
+  group("root")
+  mode("755") # u=rwx,g=rx,o=rx
+end
+directory "/var/www/drive.twolfson.com" do
+  owner("ubuntu")
+  group("ubuntu")
+  mode("755") # u=rwx,g=rx,o=rx
+end
