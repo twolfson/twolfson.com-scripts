db6ac1a6e12a7a1eb4e0d45a48c10e1c215537cf
Todd Wolfson <todd@twolfson.com>
2016-01-18 22:39:12 -0600

    Added starter recipe

---
+++ src/cookbooks/common/recipes/default.rb
@@ -0,0 +1,14 @@
+# Based on http://stackoverflow.com/a/9250482
+execute "apt-get-update-periodic" do
+  command("apt-get update")
+  only_if do
+    # If we have have ran `apt-get update` before
+    if File.exists?('/var/lib/apt/periodic/update-success-stamp')
+      # Return if we ran it in the past 24 hours
+      one_day_ago = Time.now() - (60 * 60 * 24)
+      File.mtime('/var/lib/apt/periodic/update-success-stamp') < one_day_ago
+    else
+      false
+    end
+  end
+end
