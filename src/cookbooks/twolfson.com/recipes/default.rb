3c6024f61f1d5753d9c48a64a2f06c1142d872e6
Todd Wolfson <todd@twolfson.com>
2016-01-19 02:27:49 -0600

    Fixed up one-offs

--- src/cookbooks/twolfson.com/recipes/default.rb
+++ src/cookbooks/twolfson.com/recipes/default.rb
@@ -1,12 +1,15 @@
 # Load in our dependencies
 include_recipe "common"
 
+# Define our constants
+data_dir = ENV.fetch("data_dir")
+
 # Guarantee `node` is installed
 # @depends_on execute[apt-get-update-periodic]
 # https://github.com/nodesource/distributions/tree/96e9b7d40b6aff7ade7bc130d9e18fd140e9f4f8#installation-instructions
 # DEV: Equivalent to `sudo apt-get install -y "nodejs=0.10.41-1nodesource1~trusty1"`
 execute "add-nodejs-apt-repository" do
-  execute("curl -sL https://deb.nodesource.com/setup_0.10 | sudo -E bash -")
+  command("curl -sL https://deb.nodesource.com/setup_0.10 | sudo -E bash -")
   only_if("! which node")
 end
 apt_package "nodejs" do
