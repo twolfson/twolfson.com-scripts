3077b4a43c50fe0a71783b58af69b2775a42acdb
Todd Wolfson <todd@twolfson.com>
2016-01-21 03:22:38 -0600

    Fixed locatin but things are still broken

--- src/cookbooks/common/resources/data_file.rb
+++ src/cookbooks/common/resources/data_file.rb
@@ -3,17 +3,19 @@ data_dir = ENV.fetch("data_dir")
 
 # Define an extension over `File` that auto-uses `data_dir` as `content`
 # http://stackoverflow.com/a/20732016
-class Chef
-  class Resource::DataFile < Resource::File
+module Hello
+  class DataFile
+    resource_name :data_file
+    include Resource::File
     def initialize(name, run_context=nil)
       # Run our parent constructor
-      super()
+      super(name, run_context)
 
       # If there is no content, then overwrite it
       puts name
-      if @content.nil?
-        @content = nil
-      end
+      # if @content.nil?
+      #   @content = nil
+      # end
     end
   end
 end
