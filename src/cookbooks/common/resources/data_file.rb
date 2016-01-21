e35887461c3bcd928f957e8d2a9dce96e2144373
Todd Wolfson <todd@twolfson.com>
2016-01-21 03:25:26 -0600

    Back to other format but still confused

--- src/cookbooks/common/resources/data_file.rb
+++ src/cookbooks/common/resources/data_file.rb
@@ -3,19 +3,19 @@ data_dir = ENV.fetch("data_dir")
 
 # Define an extension over `File` that auto-uses `data_dir` as `content`
 # http://stackoverflow.com/a/20732016
-module Hello
-  class DataFile
-    resource_name :data_file
-    include Resource::File
+class Chef
+  class Resource::DataFile < Resource::File
+    provides(:file)
+
     def initialize(name, run_context=nil)
       # Run our parent constructor
       super(name, run_context)
 
       # If there is no content, then overwrite it
       puts name
-      # if @content.nil?
-      #   @content = nil
-      # end
+      if @content.nil?
+        @content = nil
+      end
     end
   end
 end
