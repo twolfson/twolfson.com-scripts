188d8fee9aaacebd47f458a26fe8882db85397ce
Todd Wolfson <todd@twolfson.com>
2016-01-21 03:49:38 -0600

    Fixed up lint errors

--- src/cookbooks/common/resources/data_file.rb
+++ src/cookbooks/common/resources/data_file.rb
@@ -1,23 +1,27 @@
 # Define an extension over `File` that auto-uses `data_dir` as `content`
 # http://stackoverflow.com/a/20732016
 class Chef
-  class Provider::DataFile < Provider::File
-    provides(:data_file)
+  class Provider
+    class DataFile < Chef::Provider::File
+      provides(:data_file)
+    end
   end
 end
 
 class Chef
-  class Resource::DataFile < Resource::File
-    def initialize(name, run_context=nil)
-      # Run our parent constructor
-      super(name, run_context)
+  class Resource
+    class DataFile < Chef::Resource::File
+      def initialize(name, run_context=nil)
+        # Run our parent constructor
+        super(name, run_context)
 
-      # Load our constants
-      data_dir = ENV.fetch("data_dir")
+        # Load our constants
+        data_dir = ENV.fetch("data_dir")
 
-      # Provide default content
-      # Example: `data_dir = /vagrant`, `name = /etc/timezone` -> `/vagrant/etc/timezone`
-      self.content(::File.new("#{data_dir}#{name}").read())
+        # Provide default content
+        # Example: `data_dir = /vagrant`, `name = /etc/timezone` -> `/vagrant/etc/timezone`
+        self.content(::File.new("#{data_dir}#{name}").read())
+      end
     end
   end
 end
