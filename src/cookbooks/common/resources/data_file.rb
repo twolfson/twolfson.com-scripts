5a95bdb7910fdf7c36badec9fabb1aa84577f1c2
Todd Wolfson <todd@twolfson.com>
2016-01-21 03:31:33 -0600

    Got something working

--- src/cookbooks/common/resources/data_file.rb
+++ src/cookbooks/common/resources/data_file.rb
@@ -5,17 +5,17 @@ data_dir = ENV.fetch("data_dir")
 # http://stackoverflow.com/a/20732016
 class Chef
   class Resource::DataFile < Resource::File
-    provides(:file)
-
     def initialize(name, run_context=nil)
       # Run our parent constructor
-      super(name, run_context)
+      x = super(name, run_context)
 
       # If there is no content, then overwrite it
       puts name
       if @content.nil?
         @content = nil
       end
+
+      return x
     end
   end
 end
