9ec0f6ea3623d6203efd5ea0ac2572342778754f
Todd Wolfson <todd@twolfson.com>
2016-01-21 03:37:36 -0600

    Got everything working but seeing warning now

--- src/cookbooks/common/providers/data_file.rb
+++
@@ -1,6 +0,0 @@
-# Define an extension over `File` that auto-uses `data_dir` as `content`
-# http://stackoverflow.com/a/20732016
-class Chef
-  class Provider::DataFile < Provider::File
-  end
-end
--- src/cookbooks/common/resources/data_file.rb
+++ src/cookbooks/common/resources/data_file.rb
@@ -1,21 +1,22 @@
-# Load our constants
-data_dir = ENV.fetch("data_dir")
-
 # Define an extension over `File` that auto-uses `data_dir` as `content`
 # http://stackoverflow.com/a/20732016
+class Chef
+  class Provider::DataFile < Provider::File
+  end
+end
+
 class Chef
   class Resource::DataFile < Resource::File
     def initialize(name, run_context=nil)
       # Run our parent constructor
-      x = super(name, run_context)
+      super(name, run_context)
 
-      # If there is no content, then overwrite it
-      puts name
-      if @content.nil?
-        @content = nil
-      end
+      # Load our constants
+      data_dir = ENV.fetch("data_dir")
 
-      return x
+      # Provide default content
+      # Example: `data_dir = /vagrant`, `name = /etc/timezone` -> `/vagrant/etc/timezone`
+      self.content(::File.new("#{data_dir}#{name}").read())
     end
   end
 end
