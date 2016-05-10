5f9a903c4b133742195c25d67efdd8a3537949bf
Todd Wolfson <todd@twolfson.com>
2016-05-10 01:14:54 -0500

    Upgraded to OpenSSH@1:6.6p1-2ubuntu2.7 for security

--- test/security.rb
+++ test/security.rb
@@ -96,6 +96,6 @@ end
 # http://undeadly.org/cgi?action=article&sid=20160114142733
 describe "ssh" do
   it "is a version patched for malicious UseRoaming" do
-    expect(package("openssh-server").version).to(eq("1:6.6p1-2ubuntu2.6"))
+    expect(package("openssh-server").version).to(eq("1:6.6p1-2ubuntu2.7"))
   end
 end
