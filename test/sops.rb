27881e88a611f39bdb9e2ddd4b373774e84a6f51
Todd Wolfson <todd@twolfson.com>
2016-10-23 22:17:30 -0700

    Fixed up SOPS test

--- test/sops.rb
+++ test/sops.rb
@@ -4,8 +4,8 @@ require_relative "utils/serverspec"
 # Start our tests
 describe "SOPS" do
   it "is installed" do
-    sops_version_result = command("sops --help")
+    sops_version_result = command("sops --version")
     expect(sops_version_result.exit_status).to(eq(0))
-    expect(sops_version_result.stdout.strip()).to(eq("sops 1.14"))
+    expect(sops_version_result.stderr.strip()).to(eq("sops 1.14"))
   end
 end
