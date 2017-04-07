be4cec4d865ce7bc02bcd8c0612fbaa347a16868
Todd Wolfson <todd@twolfson.com>
2017-04-07 02:35:10 -0700

    Upgraded to Node.js@6.10.2 to fix Travis CI issues

--- test/node.rb
+++ test/node.rb
@@ -8,6 +8,6 @@ describe "Node.js" do
   end
 
   it "has our expected version" do
-    expect(command("node --version").stdout).to(eq("v6.10.1\n"))
+    expect(command("node --version").stdout).to(eq("v6.10.2\n"))
   end
 end
