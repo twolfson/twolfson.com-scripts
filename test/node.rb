408f159db8b7c9860e647f7d8b26da0847050375
Todd Wolfson <todd@twolfson.com>
2018-02-17 13:26:50 -0800

    Upgraded to Node.js@6.13.0

--- test/node.rb
+++ test/node.rb
@@ -8,6 +8,6 @@ describe "Node.js" do
   end
 
   it "has our expected version" do
-    expect(command("node --version").stdout).to(eq("v6.12.3\n"))
+    expect(command("node --version").stdout).to(eq("v6.13.0\n"))
   end
 end
