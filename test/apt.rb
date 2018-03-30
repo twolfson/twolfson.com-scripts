dbcb96fa9d0a4816c9b9c223373d477345af29e3
Todd Wolfson <todd@twolfson.com>
2018-03-29 22:45:24 -0700

    Fixed up broken test due to DateTime change

--- test/apt.rb
+++ test/apt.rb
@@ -4,8 +4,10 @@ require_relative "utils/serverspec"
 # Start our tests
 describe "apt" do
   it "was updated within past 24 hours" do
-    one_day_ago = Time.now() - 60 * 60 * 24
+    # rubocop:disable Style/DateTime
+    one_day_ago = DateTime.now() - 60 * 60 * 24
     timestamp_file = file("/var/lib/apt/periodic/update-success-stamp")
     expect(timestamp_file.mtime).to(be >= one_day_ago)
+    # rubocop:enable Style/DateTime
   end
 end
