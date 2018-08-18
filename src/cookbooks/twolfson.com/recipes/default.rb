8c406ac8387768e8a7089bed4fed07893f02552d
Todd Wolfson <todd@twolfson.com>
2018-08-18 13:59:12 -0700

    Upgraded to Node.js 6.14.4 to fix Travis CI

--- src/cookbooks/twolfson.com/recipes/default.rb
+++ src/cookbooks/twolfson.com/recipes/default.rb
@@ -3,15 +3,15 @@ include_recipe "common"
 
 # Guarantee `node` is installed
 # @depends_on execute[apt-get-update-periodic]
-# https://github.com/nodesource/distributions/tree/564ec6b1413fbfc3f2e3a47725f0abfeca678b1e#installation-instructions
-# DEV: Equivalent to `sudo apt-get install -y "nodejs=6.14.3-1nodesource1"`
+# https://github.com/nodesource/distributions/tree/564ec6.14.4fbfc3f2e3a47725f0abfeca678b1e#installation-instructions
+# DEV: Equivalent to `sudo apt-get install -y "nodejs=6.14.4-1nodesource1"`
 execute "add-nodejs-apt-repository" do
   command("curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -")
   only_if("! which node")
 end
 apt_package "nodejs" do
-  version("6.14.3-1nodesource1")
-  only_if("test \"$(node --version)\" != \"v6.14.3\"")
+  version("6.14.4-1nodesource1")
+  only_if("test \"$(node --version)\" != \"v6.14.4\"")
 end
 
 # Configure NGINX for `twolfson.com` node
--- test/node.rb
+++ test/node.rb
@@ -8,6 +8,6 @@ describe "Node.js" do
   end
 
   it "has our expected version" do
-    expect(command("node --version").stdout).to(eq("v6.14.3\n"))
+    expect(command("node --version").stdout).to(eq("v6.14.4\n"))
   end
 end
